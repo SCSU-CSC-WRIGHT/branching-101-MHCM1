a4f6c20c55705da468f29e00faa8b79a34be531d
Antonio Cavanaugh Lillo <antonio.armati.cl@gmail.com>
2025-02-23 10:01:06 -0500

    Converted code in main.py to a function.

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-"""There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
+def print_total():
+    """
+    The script below includes a function that asks the user five times 
+    for a number, adding each input number to the running total before
+    printing the result.
+    """
 
-total = 0
+    total = 0 # Intializes the total summation
+    
+    # Prompts user for input number and adds it to the sum for each iteration 
+    for i in range(5):
+        number = int(input("Enter a number: "))
+        total += number
 
-for i in range(5):
-    number = int(input("Enter a number: "))
-    total += number
+    print("The running total is: ", total)
 
-print("The running total is: ", total)
+# Defining main method for invocation 
+def main():
+    print_total()
+
+
+if __name__ == "__main__":
+    print_total()
 
